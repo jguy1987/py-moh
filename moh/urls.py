20a73d33d88779705cbecede474ea69cdb1b2ae7
jmish <jmish1987@gmail.com>
2025-05-30 20:48:20 -0500

    Commit initial layout and home files.

--- moh/urls.py
+++ moh/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from main import views as main_view
 
 urlpatterns = [
+    path('', main_view.home, name='home')
     #    path('admin/', admin.site.urls),
 ]
