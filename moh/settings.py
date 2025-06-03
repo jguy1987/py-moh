c586b8fe750e815c94dd7dedad2a40158d05f25f
jmish <jmish1987@gmail.com>
2025-06-03 13:11:02 -0500

    Switch to SQLite. MariaDB is much overkill for the scale of this project.

--- moh/settings.py
+++ moh/settings.py
@@ -76,15 +76,12 @@ WSGI_APPLICATION = 'moh.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "moh",
-        "USER": "root",
-        "PASSWORD": "j75_MYSQL!",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",  # The database file will be created in the base directory of the project
     }
 }
+
 
 
 
