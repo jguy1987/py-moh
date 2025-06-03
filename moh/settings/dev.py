849f290f8604d3269174cb5507543eaa19623fb3
jmish <jmish1987@gmail.com>
2025-06-03 13:33:58 -0500

    Switch to prod/dev settings separated.

---
+++ moh/settings/dev.py
@@ -0,0 +1,38 @@
+"""
+Development settings.
+
+In general, settings here are not suited for production.
+"""
+from moh.settings.common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-p-c)@c258u@sm5gbwb4r)%d7lrt0aq&kpphe#*6%n528t0ts2u'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",  # The database file will be created in the base directory of the project
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
